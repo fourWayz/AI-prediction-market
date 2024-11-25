bdaeb0778a36c25cacc166266ceff686db6c5c12
fourWayz <157867069+fourWayz@users.noreply.github.com>
2024-11-25 11:28:18 +0000

    install starknet and gemini dependencies

--- prediction_market_frontend/utils/connector.js
+++
@@ -1,4 +0,0 @@
-import { Provider, Contract } from "starknet";
-
-export const provider = new Provider({ network: "mainnet-alpha" });
-export const predictionMarketContract = new Contract(PredictionMarketABI, contractAddress, provider);
